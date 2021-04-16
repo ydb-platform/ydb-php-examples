01ac227c78113ca7af769be8112d4f6c64489187
Alexei Shabalin <alshabalin@yandex.ru>
2021-04-16 15:37:19 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,101 @@
+# YDB PHP Basic Example
+
+## Prerequisites
+
+- Install PHP 7.3+
+- Install Composer
+- Install PHP extensions:
+    - grpc
+    - bcmatch
+    - curl
+
+Bash commands:
+
+```bash
+sudo apt install php
+
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
+
+sudo apt install php-pear
+
+sudo pecl install grpc
+
+sudo apt install php-curl php-bcmath
+```
+
+## Installation
+
+Clone this repository.
+
+```bash
+git clone git@github.com:alshabalin/ydb-example.git
+
+cd ydb-xample
+```
+
+Copy the .env file:
+
+```bash
+cp .env.example .env
+```
+
+Edit your .env file:
+
+```
+# Common YDB settings
+DB_ENDPOINT=ydb.serverless.yandexcloud.net:2135
+DB_DATABASE=/ru-central1/b1gxxxxxxxxx/etnyyyyyyyyy
+
+# Auto discovery
+DB_DISCOVERY=false
+
+# Auth variant 1:
+# OAuth token authentication
+DB_OAUTH_TOKEN=AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+# Auth variant 2:
+# Private key authentication
+SA_PRIVATE_KEY_FILE=./private.key
+SA_ACCESS_KEY_ID=ajexxxxxxxxx
+SA_ID=ajeyyyyyyyyy
+
+# Auth variant 3:
+# Service account JSON file authentication
+SA_SERVICE_FILE=./sa_name.json
+
+# Root CA file (dedicated server only)
+YDB_SSL_ROOT_CERTIFICATES_FILE=./CA.pem
+
+# Logging settings
+USE_LOGGER=false
+```
+
+Install dependencies:
+
+```bash
+composer install
+```
+
+Or update dependencies:
+
+```bash
+composer update
+```
+
+Run the console application:
+
+```bash
+php console
+
+php console whoami
+
+php console list-endpoints
+
+php console ls
+php console mkdir test1
+php console ls test1
+php console rmdir test1
+
+php console create table1
+php console select table1
+```
---
+++ app/AppService.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App;
+
+use YandexCloud\Ydb\Ydb;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+class AppService
+{
+    const DEFAULT_ENDPOINT = 'ydb.serverless.yandexcloud.net:2135';
+
+    /**
+     * @var array
+     */
+    protected $config = [
+        'db' => [
+            'database'    => null,
+            'endpoint'    => null,
+            'discovery'   => false,
+            'iam_config'  => [
+                'key_id'             => null,
+                'service_account_id' => null,
+                'private_key_file'   => null,
+                'service_file'       => null,
+                'oauth_token'        => null,
+                'root_cert_file'     => null,
+                'temp_dir'           => null,
+            ],
+        ],
+        'use_logger' => false,
+    ];
+
+    public function __construct()
+    {
+        $this->config['db']['database']  = $_ENV['DB_DATABASE'] ?? null;
+        $this->config['db']['endpoint']  = $_ENV['DB_ENDPOINT'] ?? static::DEFAULT_ENDPOINT;
+        $this->config['db']['discovery'] = $_ENV['DB_DISCOVERY'] ?? false;
+
+        $this->config['db']['iam_config'] = [
+            'key_id'             => $_ENV['SA_ACCESS_KEY_ID'] ?? null,
+            'service_account_id' => $_ENV['SA_ID'] ?? null,
+            'private_key_file'   => $_ENV['SA_PRIVATE_KEY_FILE'] ?? null,
+            'service_file'       => $_ENV['SA_SERVICE_FILE'] ?? null,
+            'oauth_token'        => $_ENV['DB_OAUTH_TOKEN'] ?? null,
+            'root_cert_file'     => $_ENV['YDB_SSL_ROOT_CERTIFICATES_FILE'] ?? null,
+            'temp_dir'           => './tmp',
+        ];
+
+        $this->config['use_logger'] = $_ENV['USE_LOGGER'] ?? false;
+    }
+
+    /**
+     * @param string $key
+     * @return mixed|null
+     */
+    public function config($key)
+    {
+        return $this->config[$key] ?? null;
+    }
+
+    /**
+     * @return Logger|null
+     */
+    public function getLogger()
+    {
+        if ($this->config('use_logger'))
+        {
+            $logger = new Logger('ydb');
+            $logger->pushHandler(new StreamHandler('./logs/ydb-' . date('Y-m-d') . '.log'));
+            return $logger;
+        }
+        return null;
+    }
+
+    public function initYdb()
+    {
+        return new Ydb($this->config('db'), $this->getLogger());
+    }
+
+}
